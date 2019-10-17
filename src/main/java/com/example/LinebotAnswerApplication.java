8268fd3215392ca9ad3c4ae9dc85edd33fbce1cf
hiroki-okazaki <1031okazaki@gmail.com>
2019-10-17 10:37:10 +0900

    first

---
+++ src/main/java/com/example/LinebotAnswerApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinebotAnswerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinebotAnswerApplication.class, args);
+	}
+
+}
